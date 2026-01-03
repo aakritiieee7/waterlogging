b03eae7af83f6fad60e3ff3f9295f18f7786ab9d
akshaytheflash <akshaytheflash@gmail.com>
2026-01-03 23:32:28 +0530

    fnal

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 
-// Use env var if available (Vercel), otherwise fallback to hardcoded (Local)
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres.amkocqxmizilimqjegdp:eVVlVePWcPHZVDtq@aws-1-ap-south-1.pooler.supabase.com:6543/postgres';
+const connectionString = 'postgresql://postgres.amkocqxmizilimqjegdp:eVVlVePWcPHZVDtq@aws-1-ap-south-1.pooler.supabase.com:6543/postgres';
 
 const pool = new Pool({
     connectionString: connectionString,
